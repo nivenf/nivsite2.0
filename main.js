2eecbf6f32b3378361390c8e8ffc97b455cc2d13
Niven Francis <nivenf@email.arizona.edu>
2021-04-19 22:26:33 -0700

    added more things

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ var svg_dir = d3.select("#director")
 				.attr("width", WIN_WIDTH)
 				.attr("height", WIN_HEIGHT)
 				.attr("id", "d3_director")
+				.attr("position", "relative")
 				.on("mousemove", function(event, d) {
 					
 					var pageX = event.pageX;
@@ -29,23 +30,120 @@ var svg_dir = d3.select("#director")
 						return "transform: " + "rotateX(" + degreeScaleY(pageY)*1 + "deg)" + "rotateY(" + degreeScaleX(pageX)*-1 + "deg);";
 					});
 					
+		//--------------------------------------------------------------------------
+					
 					svg_dir.select("#text-port")
 					.attr("transform-origin", "center center -200px")
 					.attr("style", function() {
 						return "transform: " + "rotateX(" + degreeScaleY(pageY)*1 + "deg)" + "rotateY(" + degreeScaleX(pageX)*-1 + "deg);";
+					})
+					.on("mouseover", function(event, d) {
+						svg_dir.select("#hl-rect-port")
+						.attr("fill", "white")
+						.transition()
+						.duration(200)
+						.attr("width", "190");
+						
+						svg_dir.select("#text-port")
+						.transition()
+						.duration(200)
+						.attr("fill", "black");
+					})
+					.on("mouseout", function(event, d) {
+						svg_dir.select("#hl-rect-port")
+						.transition()
+						.duration(200)
+						.attr("width", "0");
+						
+						svg_dir.select("#text-port")
+						.transition()
+						.duration(200)
+						.attr("fill", "white");
 					});
+					
+					
 					
 					svg_dir.select("#text-about")
 					.attr("transform-origin", "center center -200px")
 					.attr("style", function() {
 						return "transform: " + "rotateX(" + degreeScaleY(pageY)*1 + "deg)" + "rotateY(" + degreeScaleX(pageX)*-1 + "deg);";
+					})
+					.on("mouseover", function(event, d) {
+						svg_dir.select("#hl-rect-about")
+						.attr("fill", "white")
+						.transition()
+						.duration(200)
+						.attr("width", "130");
+
+						svg_dir.select("#text-about")
+						.transition()
+						.duration(200)
+						.attr("fill", "black");
+					})
+					.on("mouseout", function(event, d) {
+						svg_dir.select("#hl-rect-about")
+						.transition()
+						.duration(200)
+						.attr("width", "0");
+						
+						svg_dir.select("#text-about")
+						.transition()
+						.duration(200)
+						.attr("fill", "white");
 					});
+					
+					
 					
 					svg_dir.select("#text-contact")
 					.attr("transform-origin", "center center -200px")
 					.attr("style", function() {
 						return "transform: " + "rotateX(" + degreeScaleY(pageY)*1 + "deg)" + "rotateY(" + degreeScaleX(pageX)*-1 + "deg);";
+					})
+					.on("mouseover", function(event, d) {
+						svg_dir.select("#hl-rect-contact")
+						.transition()
+						.duration(200)
+						.attr("width", "160");
+						
+						svg_dir.select("#text-contact")
+						.transition()
+						.duration(200)
+						.attr("fill", "black");
+					})
+					.on("mouseout", function(event, d) {
+						svg_dir.select("#hl-rect-contact")
+						.transition()
+						.duration(200)
+						.attr("width", "0");
+						
+						svg_dir.select("#text-contact")
+						.transition()
+						.duration(200)
+						.attr("fill", "white");
 					});
+					
+		//--------------------------------------------------------------------------
+					
+					svg_dir.select("#hl-rect-port")
+					.attr("transform-origin", "center center -200px")
+					.attr("style", function() {
+						return "transform: " + "rotateX(" + degreeScaleY(pageY)*1 + "deg)" + "rotateY(" + degreeScaleX(pageX)*-1 + "deg);";
+					});
+					
+					svg_dir.select("#hl-rect-about")
+					.attr("transform-origin", "center center -200px")
+					.attr("style", function() {
+						return "transform: " + "rotateX(" + degreeScaleY(pageY)*1 + "deg)" + "rotateY(" + degreeScaleX(pageX)*-1 + "deg);";
+					});
+					
+					svg_dir.select("#hl-rect-contact")
+					.attr("transform-origin", "center center -200px")
+					.attr("style", function() {
+						return "transform: " + "rotateX(" + degreeScaleY(pageY)*1 + "deg)" + "rotateY(" + degreeScaleX(pageX)*-1 + "deg);";
+					});
+					
+		//--------------------------------------------------------------------------
+
 				});
 
 // NIVEN FRANCIS
@@ -75,21 +173,44 @@ var GROUP_SELECTORS = svg_dir.append("g").attr("id", "GROUP_SELECTORS")
 										 .attr("fill", "White")
 										 .attr("font-smooth", "2em");
 
-var text_select = GROUP_SELECTORS.append("text")
+var highlight_port = GROUP_SELECTORS.append("rect")
+					   .attr("x", d_nf_cx - 99)
+					   .attr("y", d_nf_cy - 36)
+					   .attr("width", "0")
+					   .attr("height", "52")
+					   .attr("id", "hl-rect-port");
+					   
+var highlight_about = GROUP_SELECTORS.append("rect")
+					   .attr("x", d_nf_cx - 332)
+					   .attr("y", d_nf_cy - 36)
+					   .attr("width", "0")
+					   .attr("height", "52")
+					   .attr("id", "hl-rect-about");
+					   
+var highlight_contact = GROUP_SELECTORS.append("rect")
+					   .attr("x", d_nf_cx + 177)
+					   .attr("y", d_nf_cy - 36)
+					   .attr("width", "0")
+					   .attr("height", "52")
+					   .attr("id", "hl-rect-contact");
+
+var text_port = GROUP_SELECTORS.append("text")
 								 .attr("x", d_nf_cx - 85)
 								 .attr("y", d_nf_cy)
 								 .text("PORTFOLIO")
-								 .attr("id", "text-port");
-
+								 .attr("id", "text-port")
+								 .attr("fill", "white");
 								
-var text_port = GROUP_SELECTORS.append("text")
+var text_about = GROUP_SELECTORS.append("text")
 							   .attr("x", d_nf_cx - 320)
 							   .attr("y", d_nf_cy)
 							   .text("ABOUT")
-							   .attr("id", "text-about");
+							   .attr("id", "text-about")
+							   .attr("fill", "white");
 							   
 var text_contact = GROUP_SELECTORS.append("text")
 							   .attr("x", d_nf_cx + 190)
 							   .attr("y", d_nf_cy)
 							   .text("CONTACT")
-							   .attr("id", "text-contact");
+							   .attr("id", "text-contact")
+							   .attr("fill", "white");
